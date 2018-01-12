fa96404ca228792b0c879a667c94af6958442400
jmuldvp <jmul.dvp@gmail.com>
2018-01-11 21:52:26 -0700

    CP3 completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,15 +3,35 @@ import React, { Component } from 'react';
 class RoomList extends Component {
   constructor(props) {
     super(props);
-    this.state = { rooms: [] };
+    this.state = {
+      rooms: [],
+      newRoomName: ""
+    };
+    this.createRoom = this.createRoom.bind(this);
+    this.updateRoomNameValue = this.updateRoomNameValue.bind(this);
   }
 
   componentDidMount() {
     this.roomsRef = this.props.firebase.database().ref('rooms');
     this.roomsRef.on('child_added', snapshot => {
+      // console.log(snapshot)
       const room = snapshot.val();
       room.key = snapshot.key;
+      // console.log(room)
       this.setState({ rooms: this.state.rooms.concat( room ) });
+    });
+  }
+
+  createRoom() {
+    this.roomsRef.push({name: this.state.newRoomName});
+    this.setState({
+      newRoomName: ''
+    });
+  }
+
+  updateRoomNameValue(evt) {
+    this.setState({
+      newRoomName: evt.target.value
     });
   }
 
@@ -23,6 +43,11 @@ class RoomList extends Component {
             <li key={index}>{ room.name }</li>
           )}
         </ul>
+        <div>
+          Add room name:
+          <input value={this.state.newRoomName} onChange={this.updateRoomNameValue}/>
+          <button type="button" onClick={this.createRoom}>Create</button>
+        </div>
       </div>
     );
   }
